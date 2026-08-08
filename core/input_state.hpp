51f698edccf716eb9edf711cb6f2265827570033
ay25a <profayman2005@gmail.com>
2026-08-09 00:27:51 +0800

    - QoL changes to `vec2` including operators implementation. - Renamed `input.hpp` file to `input_enum.hpp` matching   `error_enum.hpp`. - Variables rename in `InputState` for consistency. - Methods rename in `InputState` for clarity. - Changed the type of `mouse_position` to `vec<float>` to mitigate   potential overflow, and support high DPI. - Unified data type between `mouse_position` and `mouse_position_delta`. - Removed input tests.

--- core/input_state.hpp
+++ core/input_state.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "input.hpp"
+#include "input_enum.hpp"
 #include "vec2.hpp"
 #include <bitset>
 #include <cstdint>
@@ -8,42 +8,34 @@
 namespace ye {
 class InputState {
   friend class DisplayServer;
-  /// TODO: Remove when `DisplayServer` is added
-  friend class InputStateTestAccessor;
 
  public:
-  static inline void BeginFrame() {
-    s_mouse_position_delta = {};
-    s_mouse_wheel_delta = {};
-  }
-
-  static inline void Reset() {
-    s_key_states.reset();
-    s_mouse_position = {};
-    s_mouse_position_delta = {};
-    s_mouse_wheel_delta = {};
-  }
-
-  static bool IsPressed(eInputKey key) { return s_key_states.test(static_cast<uint32_t>(key)); }
-  static inline auto GetMousePosition() noexcept { return s_mouse_position; }
-  static inline auto GetMousePositionDelta() noexcept { return s_mouse_position_delta; }
-  static inline auto GetMouseWheelDelta() noexcept { return s_mouse_wheel_delta; }
+  static bool IsPressed(eInputKey key) noexcept { return s_keys_state.test(static_cast<uint32_t>(key)); }
+  static vec2<float> GetMousePosition() noexcept { return s_mouse_position; }
+  static vec2<float> GetMousePositionDelta() noexcept { return s_mouse_position_delta; }
+  static vec2<int16_t> GetMouseWheelDelta() noexcept { return s_mouse_wheel_delta; }
 
  private:
-  static inline void SetKeyState(eInputKey key, bool isPressed) {
-    s_key_states.set(static_cast<uint32_t>(key), isPressed);
+  static void ResetDeltas() noexcept;
+  static void ResetState() noexcept;
+
+  static void UpdateKeyState(eInputKey key, bool is_pressed) noexcept {
+    s_keys_state.set(static_cast<uint32_t>(key), is_pressed);
   }
-  static inline void SetMousePosition(const vec2<uint16_t> new_pos) {
-    s_mouse_position_delta = {static_cast<int16_t>(new_pos.x - s_mouse_position.x), static_cast<int16_t>(new_pos.y - s_mouse_position.y)};
+
+  static void UpdateMousePosition(vec2<float> new_pos) noexcept {
+    s_mouse_position_delta = new_pos - s_mouse_position;
     s_mouse_position = new_pos;
   }
 
-  static inline void SetMouseWheelDelta(const vec2<int16_t> new_delta) { s_mouse_wheel_delta = new_delta; }
+  static void UpdateMouseWheelDelta(vec2<int16_t> new_delta) noexcept {
+    s_mouse_wheel_delta = new_delta;
+  }
 
  private:
-  static std::bitset<static_cast<uint32_t>(eInputKey::Count)> s_key_states;
-  static vec2<uint16_t> s_mouse_position;
-  static vec2<int16_t> s_mouse_position_delta;
+  static std::bitset<static_cast<uint32_t>(eInputKey::Count)> s_keys_state;
+  static vec2<float> s_mouse_position;
+  static vec2<float> s_mouse_position_delta;
   static vec2<int16_t> s_mouse_wheel_delta;
 };
 }  // namespace ye
--- core/vec2.hpp
+++ core/vec2.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <compare>
 #include <concepts>
 
 template <typename _NumT>
@@ -8,5 +9,40 @@ struct vec2 {
   _NumT x = 0;
   _NumT y = 0;
 
-  auto operator<=>(const vec2<_NumT>&) const = default;
+  vec2(_NumT x, _NumT y) noexcept {
+    this->x = x;
+    this->y = y;
+  }
+
+  vec2() noexcept = default;
+  vec2(const vec2&) noexcept = default;
+  vec2& operator=(const vec2&) noexcept = default;
+
+  vec2(vec2&&) noexcept = default;
+  vec2& operator=(vec2&&) noexcept = default;
+  ~vec2() noexcept = default;
+
+  std::strong_ordering operator<=>(const vec2<_NumT>&) const noexcept = default;
+
+  vec2 operator-(const vec2 rhs) const noexcept {
+    return vec2(this->x - rhs.x, this->y - rhs.y);
+  }
+
+  vec2 operator+(const vec2 rhs) const noexcept {
+    return vec2(this->x + rhs.x, this->y + rhs.y);
+  }
+
+  vec2& operator-=(const vec2& rhs) noexcept {
+    x -= rhs.x;
+    y -= rhs.y;
+
+    return *this;
+  }
+
+  vec2<_NumT>& operator+=(const vec2& rhs) noexcept {
+    x += rhs.x;
+    y += rhs.y;
+
+    return *this;
+  }
 };
